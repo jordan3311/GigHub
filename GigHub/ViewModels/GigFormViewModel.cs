db4a4b3096a124586afda8fad37381ca2157f612
Jordan Johnson <Jordan Johnson>
2017-04-09 22:09:44 -0400

    Create add a gig form

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+
+        //added for a drop list used IEnumable because we are not changing thes list 
+        public IEnumerable<Genre> Genres { get; set; } 
+    }
+}
